f627452c57f412652bc71f4f4c543aa6ed39a6e6
SARASWATHIBAS <saraswathibas@gmail.com>
2026-04-25 22:27:33 -0400

    Clean up requests import and unused code
    Removed unused requests import and related code.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages.
 import streamlit as st
+import requests  
 
 from snowflake.snowpark.functions import col
 
@@ -50,10 +51,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered!', icon="✅")
     #st.write(my_insert_stmt)
 
-import requests  
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-
-#st.text(smoothiefroot_response.json())
-sf_df=st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
 
